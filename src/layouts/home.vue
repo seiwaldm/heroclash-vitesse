72e1e02470fac5baa24e073b906f019b10d48f3f
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-10-25 20:52:12 +0200

    added sound to animation

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,9 +1,6 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <main flex flex-col justify-center items-center h="100vh">
     <RouterView />
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
-      [Home Layout]
-    </div>
   </main>
 </template>
+

14748cbf9da3a7c5450ecb9d008df2ccaacfe604
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-03 21:01:47 +0100

    switched logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <RouterLink flex justify-center max-w="90vw" to="/menu" my-5>
-    <img src="/heroclash-logo-lang-optimized.svg" alt="Heroclash-Logo" width="400">
+    <img src="/heroclash-logo-lang.png" alt="Heroclash-Logo" width="400">
   </RouterLink>
 </template>

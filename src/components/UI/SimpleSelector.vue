fb0ff5220228375c40c292119908e8c301bc7530
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-06 10:34:32 +0100

    added HeroFilter
    TODO: return default hero if filtered list is empty

--- src/components/UI/SimpleSelector.vue
+++ src/components/UI/SimpleSelector.vue
@@ -6,7 +6,7 @@ const props = defineProps({
     default: '1',
   },
   min: String,
-  max: String,
+  max: Number,
 })
 const emits = defineEmits(['update'])
 const counter = ref(props.initValue)
@@ -23,6 +23,17 @@ function countDown() {
     emits('update', counter.value)
   }
 }
+
+function updateCounter(event) {
+  if (event.target.value >= props.min && event.target.value <= props.max) {
+    counter.value = event.target.value
+    emits('update', counter.value)
+  }
+  else {
+    counter.value = props.max
+    emits('update', counter.value)
+  }
+}
 </script>
 
 <template>
@@ -32,7 +43,7 @@ function countDown() {
     </h1>
     <div flex m-3 justify-center>
       <span hc-font-style hover:scale comic-kings text-15 cursor-pointer select-none @click="countDown">&lt;</span>
-      <input v-model="counter" type="number" :min="min" :max="max" hidden-controls text-center bg-transparent hc-font-style action-comics text-7 w="200px" @input="emits('update', counter)">
+      <input v-model="counter" type="number" :min="min" :max="max" hidden-controls text-center bg-transparent hc-font-style action-comics text-7 w="200px" @input="updateCounter">
       <span hc-font-style hover:scale comic-kings text-15 cursor-pointer select-none @click="countUp">&gt;</span>
     </div>
   </div>
--- src/pages/gallery/index.vue
+++ src/pages/gallery/index.vue
@@ -3,73 +3,28 @@ import db from '~/database/db.js'
 import { useCardsStore } from '~/store/cards'
 
 const cardStore = useCardsStore()
-
-const hero = ref({
-  appearance: {
-    eyeColor: 'Yellow',
-    gender: 'Male',
-    hairColor: 'No Hair',
-    height: [
-      '6\'8',
-      '203 cm',
-    ],
-    race: 'Human',
-    weight: [
-      '980 lb',
-      '441 kg',
-    ],
-  },
-  biography: {
-    aliases: [
-      'Rick Jones',
-    ],
-    alignment: 'good',
-    alterEgos: 'No alter egos found.',
-    firstAppearance: 'Hulk Vol 2 #2 (April, 2008) (as A-Bomb)',
-    fullName: 'Richard Milhouse Jones',
-    placeOfBirth: 'Scarsdale, Arizona',
-    publisher: 'Marvel Comics',
-  },
-  connections: {
-    groupAffiliation: 'Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom',
-    relatives: 'Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)',
-  },
-  id: 1,
-  images: {
-    lg: 'images/lg/1-a-bomb.jpg',
-    md: 'images/md/1-a-bomb.jpg',
-    sm: 'images/sm/1-a-bomb.jpg',
-    xs: 'images/xs/1-a-bomb.jpg',
-  },
-  name: 'A-Bomb',
-  powerstats: {
-    combat: 64,
-    durability: 80,
-    intelligence: 38,
-    power: 24,
-    speed: 17,
-    strength: 100,
-  },
-  slug: '1-a-bomb',
-  work: {
-    base: '-',
-    occupation: 'Musician, adventurer, author; formerly talk show host',
-  },
+const counter = ref(0)
+const filter = ref('')
+const filteredHeroList = computed(() => {
+  return cardStore.cards.map(card => card.data).filter(hero => hero.name.toLowerCase().includes(filter.value.toLowerCase()))
+})
+const hero = computed(() => {
+  return filteredHeroList.value[counter.value]
 })
 
 const countHeroes = computed(() => {
-  return cardStore.cards.length
+  return filteredHeroList.value.length
 })
 
-function updateHero(counter) {
-  hero.value = cardStore.cards[counter - 1].data
+function updateHero(newCounter) {
+  counter.value = newCounter - 1
 }
 </script>
 
 <template>
-  <div flex flex-col items-center>
+  <div flex flex-col gap-1 items-center>
     <SimpleSelector name="Hero Nr." min="1" :max="countHeroes" @update="updateHero" />
-
+    <div><input v-model="filter" type="text" placeholder="filter Heroes by Name..." b="1" rounded></div>
     <HeroCard :hero="hero" my-4 />
     <div>(Click on Image for Character Details)</div>
   </div>

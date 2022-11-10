4c58649f61bb737a421b50046d648c214b598ac9
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-10 22:26:56 +0100

    fixed redirect with login via mail/pw

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+
 const userStore = useUserStore()
 const router = useRouter()
 
+const { user } = storeToRefs(userStore)
+
 if (userStore.user)
   router.push(`/user/${userStore.user.id}`)
+
+watch(user, () => {
+  if (userStore.user)
+    router.push(`/user/${userStore.user.id}`)
+})
 </script>
 
 <template>

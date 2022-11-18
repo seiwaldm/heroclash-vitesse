843c3a3e37e8daa80d9217cf29570d4d4b03f1ff
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-18 18:56:51 +0100

    Update GameLog.vue

--- src/components/GameComponents/GameLog.vue
+++ src/components/GameComponents/GameLog.vue
@@ -1,6 +1,10 @@
 <script setup>
 const props = defineProps(['log'])
 const emits = defineEmits(['close'])
+const list = ref(null)
+watch (props.log, (newVal) => {
+  list.value.scrollTop = list.value.scrollTopMax
+})
 
 function icon(discipline) {
   switch (discipline) {
@@ -22,15 +26,15 @@ function icon(discipline) {
 
 <template>
   <div>
-    <DragResize rounded-1 p-2 :w="400" class="container" z-10>
+    <DragResize v-if="log" rounded-1 p-2 :w="360" class="container" z-10>
       <div absolute right="3" top="1" cursor-pointer @click="emits('close')">
         X
       </div>
       <div text-5>
         Game Log
       </div>
-      <div flex flex-col items-center max-h="85%" overflow-auto>
-        <div v-for="(entry, index) in log" :key="index" class="log" w="100%" my-3>
+      <div ref="list" flex flex-col items-center max-h="80%" max-w="100%" overflow-auto gap-2 pb-10>
+        <div v-for="(entry, index) in log" :key="index" class="log" w="100%">
           <div flex gap-4 items-center justify-between w="100%">
             <div>{{ index + 1 }}</div>
             <div flex gap-2 items-center justify-center w="100%">
@@ -55,5 +59,9 @@ function icon(discipline) {
     filter: drop-shadow(15px 15px 15px);
     backdrop-filter: blur(10px);
     color: #374151
+}
+
+.log{
+  line-height: 1.1rem;
 }
 </style>

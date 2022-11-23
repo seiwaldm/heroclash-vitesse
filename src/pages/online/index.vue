86aafd3ad633c0f2d0c7594e5d49d462a5a18d01
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-23 23:19:54 +0100

    update about

---
+++ src/pages/online/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { db } from '~/database/db'
+
+const gameCreator = ref(false)
+</script>
+
+<template>
+  <p>Active Games:</p>
+
+  <button hc-font-style action-comics hover:scale text-5 @click="gameCreator = true">
+    New Online Game
+  </button>
+</template>
